47e8ef6963c7881f8d90f5138fc1e513aada0cc9
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2023-11-16 14:30:31 +0530

    hero.tsx for service

--- app/routes/services.MobileDev.tsx
+++ app/routes/services.MobileDev.tsx
@@ -26,17 +26,25 @@ import ServiceCardContainer from "~/components/S-MobileAppDev/section-6/service-
 import { Outlet } from "@remix-run/react";
 
 export async function loader() {
-  const res = await fetch( strapiUrl+"/api/healthcare/?populate=%2A")
+  const res = await fetch( strapiUrl+"/api/s-mad?populate=%2A")
+  const componentRes = await fetch( strapiUrl+"/api/s-mad/?populate=s2_keyPoints.keyPointsImage,s5_phasesOfDevelopment.s5_phasesImage,s7_techIcons.s7_techIcon,s6_serviceCard.s6_serviceCardImage,s4_industryFocus.s4_IndustryFocusImage")
   let jsonParsed = await res.json();
+  let jsonParsed2 = await componentRes.json();
   return {
+    heroImage: jsonParsed.data.attributes.heroImage.data.attributes.formats.large.url,
     heroTitle: jsonParsed.data.attributes.heroTitle,
-    section2Title: jsonParsed.data.attributes.section_2_title,
-    section2Image: jsonParsed.data.attributes.section_2_image.data.attributes.url,
-    section2Desc : jsonParsed.data.attributes.section_2_description,
-    section3Title :jsonParsed.data.attributes.section_3_title,
-    section3Desc : jsonParsed.data.attributes.section_3_description,
-    section3Tags : jsonParsed.data.attributes.section_3_tags,
-    section3Image : jsonParsed.data.attributes.section_3_image.data.attributes.formats.medium.url
+    heroDescription: jsonParsed.data.attributes.heroDescription,
+    s2_Title : jsonParsed.data.attributes.s2_Title,
+    s2_Description :jsonParsed.data.attributes.s2_Description,
+    s3_countryCount : jsonParsed.data.attributes.s3_countryCount,
+    s3_projectDelieverdCount : jsonParsed.data.attributes.s3_projectDelieverdCount,
+    s3_TotalProjectCount : jsonParsed.data.attributes.s3_TotalProjectCount,
+    s4_industryFocusTitle: jsonParsed.data.attributes.s4_industryFocusTitle,
+    s5_title: jsonParsed.data.attributes.s5_title,
+    s6_serviceTitle : jsonParsed.data.attributes.s6_serviceTitle,
+    s6_serviceSummary :jsonParsed.data.attributes.s6_serviceSummary,
+    s7_techTitle : jsonParsed.data.attributes.s7_techTitle,
+    
   };
 }
 const MobDev = () => {
