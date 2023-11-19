01cbf20fb28cb2d4fc7096c2412ab69374b6a742
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2023-11-20 01:04:33 +0530

    service enhancement

--- app/components/S-MobileAppDev/section-6/service-card.tsx
+++ app/components/S-MobileAppDev/section-6/service-card.tsx
@@ -1,23 +1,24 @@
 import { strapiUrl } from "~/utils/urls";
 const ServiceCard = ({ service }: { service: any }) => {
-    // Extract data from the blog object
-    console.log(service,"is the blog data passsedddd")
-  
-    return(
-       <div className="blog-card z-10  min-w-[200px] lg:h-[220px] gap-x-6 relative justify-around whitespace-nowrap">
-            <div className="px-2  object-contain justify-start bg-blue-600">
-                 <img src={service.s6_serviceCardImage} className="p-4 object-contain w-full"></img> 
+    
+    return (
+        <div className="blog-card justify-around">
+            <div className=" " style={{ flexBasis: '40%', height: '140px' }}>
+                <img
+                    src={service.s6_serviceCardImage}
+                    className="p-4  w-full h-full"
+                    alt={service.s6_serviceCardTitle}
+                />
             </div>
-      
-            <div className="px-4 text-white font-oxygen font-medium text-xl justify-start">
-                <span className="no-wrap">{service.s6_serviceCardTitle}</span>
+
+            <div className=" px-4 text-white font-oxygen font-medium text-xl justify-start">
+                <span className="">{service.s6_serviceCardTitle}</span>
             </div>
-           
-            <div className="flex text-white font-light font-montserrat text-xs w-full justify-between p-4">
-                <span className="no-wrap service-card-summary">{service.s6_serviceCardDescription}</span>
+
+            <div className="py-4 text-white font-light font-montserrat text-xs w-full justify-between p-4">
+                <span className="service-card-summary">{service.s6_serviceCardDescription}</span>
             </div>
-            
-       </div> 
+        </div>
     );
-}
+};
 export default ServiceCard;
