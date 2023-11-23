206e27b69aa9f80cd179eb151a855c9953c37f5f
bhavish <bhavishp@ivoyant.com>
2023-11-23 11:22:26 +0530

    thursday commit

---
+++ app/utils/fetchdata.ts
@@ -0,0 +1,17 @@
+import { strapiUrl } from "./urls";
+
+export async function fetchData(endpoint:string) {
+    try {
+      const response:Response = await fetch(strapiUrl + endpoint);
+  
+      if (!response.ok) {
+        throw new Error(`Error fetching data from ${endpoint}: ${response.status} ${response.statusText}`);
+      }
+  
+      const jsonData = await response.json();
+      return jsonData.data.attributes;
+    } catch (error: any ) {
+      console.error(`Error fetching data from ${endpoint}: ${error.message}`);
+      throw error;
+    }
+  }
