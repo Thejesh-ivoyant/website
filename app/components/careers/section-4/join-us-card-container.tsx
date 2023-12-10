5472091d30927728cfe49ea3999898ff3b91f186
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2023-12-11 00:29:33 +0530

    join us card

--- app/components/careers/section-4/join-us-card-container.tsx
+++ app/components/careers/section-4/join-us-card-container.tsx
@@ -13,12 +13,12 @@ const JoinUsCardContainer = () => {
 
      
       <div className="w-full  flex flex-col p-4">
-        <div className="grid grid-cols-3 self-center h-fit gap-4 p-2">
+        <div className="flex justify-between flex-row self-center h-fit gap-8 p-2">
           {loaderData.DescriptionCard.map(
             (item: { id: React.Key | null | undefined }, index: any) => (
               <div
                 key={item.id}
-                className="flex flex-row justify-around w-96 aspect-square object-cover"
+                className="grid col-span-1 w-96 aspect-square object-cover"
               >
                 <JobCard service={item} />
               </div>
