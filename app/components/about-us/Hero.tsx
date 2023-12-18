8ed27295a623383281f5a69342cf51df3902df40
bhavish <bhavishp@ivoyant.com>
2023-12-18 17:32:06 +0530

    few changes in styles and assets

--- app/components/about-us/Hero.tsx
+++ app/components/about-us/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
       <div className="relative flex mx-auto max-w-2xl text-center items-center justify-center flex-col gap-4" >
         <h1 className="font-semibold font-montserrat text-4xl">{data.heroTitle}</h1>
         <svg width="888" height="25" viewBox="0 0 888 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path transform="rotate(-90 0 12.5)" fill="url(#a)" d="M0 12.5h1v444H0z"/><path transform="rotate(90 888 11.5)" fill="url(#b)" d="M888 11.5h1v444h-1z"/><defs><linearGradient id="a" x1="1.5" y1="453.191" x2="1.5" y2="4.56" gradientUnits="userSpaceOnUse"><stop stopColor="#AEBEFF"/><stop offset="1" stopColor="#A7B8FE" stopOpacity="0"/></linearGradient><linearGradient id="b" x1="889.5" y1="452.191" x2="889.5" y2="3.56" gradientUnits="userSpaceOnUse"><stop stopColor="#AEBEFF"/><stop offset="1" stopColor="#A7B8FE" stopOpacity="0"/></linearGradient></defs></svg>
-        <div className="">
+        <div className=" font-poppins">
           <span>
           {data.heroDescription}
           </span>
