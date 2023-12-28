444bc1baf0f53ea59f7ab39fbb5a1ef3299991a4
bhavish <bhavishp@ivoyant.com>
2023-12-28 22:10:16 +0530

    bug fix and background shadow for the slides

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,7 +18,6 @@ import { ActionFunction } from "@remix-run/node";
 import ErrorBoundary from "~/components/ErrorBoundary";
 import ContactUs from "~/common-components/contactUs";
 import LoadingTest from "~/common-components/loading-test";
-import Section2 from "~/components/products/section2";
 import { Attributes } from "~/interfaces/Homepage";
 import WhyChooseUs from "~/components/Homepage/why-choose-us";
 export const meta: MetaFunction = () => {
@@ -83,7 +82,7 @@ const App = () => {
       <Testimonials/>
       <BlogPostsContainer />
       <WhyChooseUs pairs={attributes.pairs} title={attributes.whychooseus} description={attributes.whychooseusDesc} />
-      {/* <ContactUs /> */}
+      <ContactUs />
       <Footer />
     </>
   );
