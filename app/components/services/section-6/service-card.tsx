841431aeec336a9795ceeecf88dd410b04516918
bhavish <bhavishp@ivoyant.com>
2024-01-02 16:42:37 +0530

    cards fixed resolution in services page

--- app/components/services/section-6/service-card.tsx
+++ app/components/services/section-6/service-card.tsx
@@ -2,15 +2,15 @@ import { strapiUrl } from "~/utils/urls";
 const ServiceCard = ({ service }: { service: any }) => {
     
     return (
-        <div className="blog-card flex flex-col w-full h-full">
+        <div className="blog-card flex flex-col w-full h-full overflow-hidden">
              <img
                     src={service.s6_serviceCardImage}
                     className="w-full h-[50%] object-cover"
                     alt={service.s6_serviceCardTitle}
                 />
-            <div className="text-white p-4 flex flex-col h-full w-full">
-                <p className="flex mt-4 w-full text-left text-xl font-semibold leading-7 font-montserrat">{service.s6_serviceCardTitle}</p>
-                <p className="flex font-thin text-xs font-poppins my-auto">{service.s6_serviceCardDescription}</p>
+            <div className="text-white p-4 flex flex-col h-full w-full gap-y-4">
+                <p className="h-14 aspect[6/1] flex w-full text-left mr-auto text-xl font-semibold leading-7 font-montserrat">{service.s6_serviceCardTitle}</p>
+                <p className="flex font-thin text-xs font-poppins">{service.s6_serviceCardDescription}</p>
             </div>
         </div>
     );
