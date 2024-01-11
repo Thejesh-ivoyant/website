32eaa3ec423b464808f95721955992df039b708b
bhavish <bhavishp@ivoyant.com>
2024-01-12 01:40:19 +0530

    Privacy page hero fixed

--- app/components/policy-terms-cookies/ptc-hero.tsx
+++ app/components/policy-terms-cookies/ptc-hero.tsx
@@ -1,36 +1,48 @@
 import { useLoaderData } from "@remix-run/react";
-
+import heroImage from '~/../public/assets/policy.jpg'
 const PTCHero = () => {
   const loaderData = useLoaderData() as any;
-
+  const gradientStyle = {
+    zIndex: -1,
+    background: `linear-gradient(100deg, white 50%, transparent 50%), url(${heroImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
   return (
-    <div>
-       <div className="bg-slate-50 w-full max-md:max-w-full">
-        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-          <div className="flex flex-col items-stretch w-[47%] max-md:w-full max-md:ml-0">
+    <>
+    <div  className="relative screen-height mt-20 text-white flex flex-row" style={gradientStyle}>
+      <div className="w-1/2 grid place-items-center">
+        <div className="privacy-hero-text flex flex-col my-auto max-w-lg">
+          <small className="text-lg font-poppins leading-8 font-normal">Last reviewed on: Dec 09, 2023</small>
+          <h1 className="font-montserrat text-4xl font-extrabold text-haiti italic">iVoyant Web Privacy policy</h1>
+          <p className="font-poppins text-lg font-normal leading-8 text-justify mt-5">
+          At iVoyant, safeguarding your privacy is our top priority. Committed to responsible data processing, we adhere to global data protection laws. Our Privacy Statement outlines how we handle personal information collected from consumers, clients, and business partners. Trust iVoyant for secure and compliant data practices.
+          </p>
+        </div> 
+      </div>     
+    </div>
+    </>
+  );
+};
+
+export default PTCHero;
+
+{/* <div className="flex flex-col items-stretch w-1/2 bg-white">
             <div className="flex-col fill-white overflow-hidden relative flex min-h-[691px] grow items-center px-20 py-12 max-md:max-w-full max-md:px-5">
              
               <div className="relative text-indigo-950 text-opacity-80 text-xl leading-8 whitespace-nowrap mt-24 max-md:max-w-full max-md:mt-10">
                 Last reviewed on: {loaderData.last_reviewed}
               </div>
               <div className="relative text-violet-950 text-4xl italic font-extrabold leading-[58px] w-[504px] max-w-full mt-1">
-            {loaderData.heroTitle}
+              {loaderData.heroTitle}
               </div>
               <div className="relative text-indigo-950 text-xl leading-8 w-[504px] max-w-full mt-5 mb-16 max-md:mb-10">
               {loaderData.heroDescription}
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-stretch w-[53%] ml-5 max-md:w-full max-md:ml-0">
-            <img
-              loading="lazy"
-              src= '../assets/policy.jpg'              className="aspect-[1.3] object-contain object-center w-full overflow-hidden grow max-md:max-w-full"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
-export default PTCHero;
+<img
+loading="eager"
+src= '../assets/policy.jpg'              className="object-contain h-full -z-10 right-0 top-0"
+/> */}
