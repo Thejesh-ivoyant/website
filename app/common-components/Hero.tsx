648bca0d15c4d8169d0d80a1cce07b73af84760f
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-01-14 16:03:35 +0530

    image fitted

--- app/common-components/Hero.tsx
+++ app/common-components/Hero.tsx
@@ -1,14 +1,14 @@
 import { Outlet, useLoaderData } from "@remix-run/react";
-import { scrollTo, scrollToSection } from "~/root";
+import { scrollToSection } from "~/root";
 const Hero = () => {
 
   const loaderData = useLoaderData() as any;
 
   const gradientStyle = {
-    background: `linear-gradient(180deg, rgba(0, 0, 0, 0.60) 0%, rgba(0, 0, 0, 0.60) 66.95%, rgba(0, 0, 0, 0.00) 152.46%), url(${loaderData.heroBgImageURl}) lightgray 50% / cover no-repeat`,
+    background: `linear-gradient(180deg, rgba(0, 0, 0, 0.60) 0%, rgba(0, 0, 0, 0.60) 66.95%, rgba(0, 0, 0, 0.00) 152.46%), url(${loaderData.heroBgImageURl}) lightgray 50% / 100% 100%  no-repeat`,
   };
   return (
-    <section className="hero min-h-screen text-white flex justify-center object-cover" style={gradientStyle}>
+    <section className="mt-[4.5rem] screen-height text-white flex justify-center w-full " style={gradientStyle}>
       
       <div className="relative flex mx-auto max-w-2xl text-center items-center justify-center flex-col gap-4" >
         <h1 className="font-semibold font-montserrat text-4xl">{loaderData.heroTitle}</h1>
