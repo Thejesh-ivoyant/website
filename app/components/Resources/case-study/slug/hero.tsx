d100651e1b2105689d400b4a8f85d1a129d9dd91
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-01-14 17:09:36 +0530

    strech to cover

--- app/components/Resources/case-study/slug/hero.tsx
+++ app/components/Resources/case-study/slug/hero.tsx
@@ -1,6 +1,6 @@
 const Hero = ({data}:{data:any}) => {
   const gradientStyle = {
-    background: `linear-gradient(180deg, rgba(0, 0, 0, 0.60) 0%, rgba(0, 0, 0, 0.60) 66.95%, rgba(0, 0, 0, 0.00) 152.46%), url(${data?.heroBgImage?.data.attributes.url}) lightgray 50% / 100% 100% no-repeat`,
+    background: `linear-gradient(180deg, rgba(0, 0, 0, 0.60) 0%, rgba(0, 0, 0, 0.60) 66.95%, rgba(0, 0, 0, 0.00) 152.46%), url(${data?.heroBgImage?.data.attributes.url}) lightgray 50% /cover no-repeat`,
   };
   return (
     <>
