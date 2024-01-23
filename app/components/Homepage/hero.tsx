e361a8437cd3d1bd69a270c7af2fa5cfd8940bd5
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-01-23 15:31:27 +0530

    animated hero title

--- app/components/Homepage/hero.tsx
+++ app/components/Homepage/hero.tsx
@@ -36,20 +36,10 @@ const Hero = ({
         />
        
 <div className="hero-wrapper">
-<div className="hero-title  animated-text">
-                
-                {heroText?.split(" ").map((word, wordIndex) => (
-                  <React.Fragment key={wordIndex}>
-                    {word.split("").map((char, charIndex) => (
-                      <span key={charIndex}>
-                        {char}
-                      </span>
-                    ))}
-                    <span>&nbsp;</span>
-                  </React.Fragment>
-                ))}
+<p className="hero-title  animated-text">              
+               {heroText}
               
-                </div>
+                </p>
                 <h1 className="hero-title font-bold tracking-[0.0969rem]">{heroTitle}</h1>
               
               <svg  width="100%" height="25" viewBox="0 0 888 25" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="12.5" width="1" height="444" transform="rotate(-90 0 12.5)" fill="url(#paint0_linear_1695_48769)"/><rect x="888" y="11.5" width="1" height="444" transform="rotate(90 888 11.5)" fill="url(#paint1_linear_1695_48769)"/><defs><linearGradient id="paint0_linear_1695_48769" x1="1.5" y1="453.191" x2="1.5" y2="4.55961" gradientUnits="userSpaceOnUse"><stop stopColor="#AEBEFF"/><stop offset="1" stopColor="#A7B8FE" stopOpacity="0"/></linearGradient><linearGradient id="paint1_linear_1695_48769" x1="889.5" y1="452.191" x2="889.5" y2="3.55961" gradientUnits="userSpaceOnUse"><stop stopColor="#AEBEFF"/><stop offset="1" stopColor="#A7B8FE" stopOpacity="0"/></linearGradient></defs></svg>
--- app/components/Homepage/summary-card.tsx
+++ app/components/Homepage/summary-card.tsx
@@ -6,25 +6,25 @@ const SummaryCard = ({attributes}:{attributes:Attributes}) => {
     <div className="relative screen-height flex items-center  justify-center font-montserrat p-2 overflow-hidden opacity-95">
       <div className="moving-bg"></div>
         <div className="grid grid-cols-2 aspect-square w-fit m-4">
-          <div className="aspect-square inline-flex justify-center items-center col-span-1 grape-shadow">
+          <div className="aspect-square inline-flex justify-center items-center col-span-1 ">
             <div className="flex w-fit gap-6 p-10">
               <div className="text-7xl font-extrabold ">{attributes.ClientCount}</div>
               <div className="text-xl h-12 aspect-[19/8] my-auto leading-none">Delighted Clients</div>
             </div>
           </div>
-          <div className="aspect-square inline-flex justify-center border-container-top items-center col-span-1 grape-shadow">
+          <div className="aspect-square inline-flex justify-center border-container-top items-center col-span-1 ">
             <div className="flex w-fit gap-6 p-10">
               <div className="text-7xl font-extrabold ">{attributes.ExperienceCount}</div>
               <div className="text-xl h-12 aspect-[19/8] my-auto leading-none">Years of Experience</div>
             </div>
           </div>
-          <div className="aspect-square inline-flex justify-center border-container-left  items-center col-span-1 grape-shadow">
+          <div className="aspect-square inline-flex justify-center border-container-left  items-center col-span-1 ">
             <div className="flex w-fit gap-6 p-10">
               <div className="text-7xl font-extrabold ">{attributes.ProjectsCount}</div>
               <div className="text-xl h-12 aspect-[19/8] my-auto leading-none">Successful Projects</div>
             </div>
           </div>
-          <div className="aspect-square inline-flex justify-center border-container-bottom items-center col-span-1 grape-shadow">
+          <div className="aspect-square inline-flex justify-center border-container-bottom items-center col-span-1 ">
             <div className="flex w-fit gap-6 p-10">
               <div className="text-7xl font-extrabold ">{attributes.InHouseExpertsCount}</div>
               <div className="text-xl h-12 aspect-[19/8] my-auto leading-none">In-House Experts Count</div>
