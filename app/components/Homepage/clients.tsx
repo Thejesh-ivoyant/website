3c261855af3259d6bb1bf1fbe1a1931e52a7a422
bhavish <bhavishp@ivoyant.com>
2024-02-05 19:21:21 +0530

    clients/parterns section fixed in homepage and is responsive

--- app/components/Homepage/clients.tsx
+++ app/components/Homepage/clients.tsx
@@ -4,7 +4,7 @@ import line from '../../../public/assets/pur-line.svg'
 const Section4 = ({ clients }: { clients: Clients | undefined }) => {
   return (
     <>
-      <div className="text-PurpBlue font-montserrat font-medium text-4xl text-center p-10 tracking-wider">
+      <div className="text-PurpBlue font-montserrat font-medium md:text-3xl sm:text-2xl lg:text-4xl text-center md:p-8 sm:p-6 p-5 lg:p-10 tracking-wider">
         <h1>Satisfied clients</h1>
       </div>
       <img className= "mx-auto" src = {line}/>
@@ -12,6 +12,12 @@ const Section4 = ({ clients }: { clients: Clients | undefined }) => {
         <div className="logos-slide whitespace-nowrap">
           {clients?.data.map((logo, index: number) => (
             <img src={`${logo.attributes.url}`} alt="Client Logo" key={index} className="gradient-left grayscale hover:grayscale-0" />
+          ))}
+        </div>
+
+        <div className="logos-slide">
+          {clients?.data.map((logo, index: number) => (
+            <img src={`${logo.attributes.url}`} alt="Client Logo"  key={index}  className="gradient-left grayscale hover:grayscale-0" />
           ))}
         </div>
 
