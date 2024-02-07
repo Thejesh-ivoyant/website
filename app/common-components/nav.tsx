a0ddd682e34fd507c8a88c15c582a7e11207937f
bhavish <bhavishp@ivoyant.com>
2024-02-07 11:05:35 +0530

    responsive navbar

--- app/common-components/nav.tsx
+++ app/common-components/nav.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import Sidebar from "./sidebar";
 import { Form, Link, useMatch, useRouteLoaderData } from "@remix-run/react";
 import { Modal } from "antd";
-
+import fb from '~/../public/assets/Facebook svg.svg'
+import twitter from '~/../public/assets/og-twitter.svg'
+import linkedin from '~/../public/assets/Linkedin-white.svg'
+import yt from '~/../public/assets/YouTube svg.svg'
 import ivurl from '../../public/assets/ivoyant.svg';
 import defaultsvg from '../../public/assets/default.svg';
 import { success,errorMessage } from "~/utils/notifications";
@@ -19,6 +22,10 @@ const Nav = () => {
   const [open, setOpen] = useState(false);
   const [download, setDownload] = useState<string>("");
   const [toggleNav, setToggleNav] = useState<boolean>(false);
+  const [clicked, setClicked] = useState<number>(-1);
+  const handleToggle = (index:number) =>{
+    (index === clicked) ? setClicked(-1) : setClicked(index)
+  }
   const handleClick = () => {
     setToggleNav(!toggleNav);
 
@@ -73,7 +80,7 @@ const Nav = () => {
   };
 
   const handleHamburgerClick = () => {
-    setSidebarOpen(!sidebarOpen);
+    setSidebarOpen((prevSidebarOpen) => !prevSidebarOpen);
   };
   const handleCancel = () => {
     setOpen(false);
@@ -105,7 +112,7 @@ const Nav = () => {
     "company",
   ];
   return (
-    <div>
+    <>
        <Modal
         open={open}
         title="Download PitchDeck"
@@ -155,7 +162,7 @@ const Nav = () => {
     </div>
   </Form>
       </Modal>
-      <nav className="fixed top-0 z-50 w-full bg-nav-dark pt-2 pb-1">
+      <nav className="fixed top-0 z-50 w-full bg-nav-dark pt-2 pb-1 lg:block hidden">
         <div className="flex flex-row items-center justify-around">
           <Link to="/">
             {" "}
@@ -195,14 +202,14 @@ const Nav = () => {
                   <div className="px-2 pt-2 pb-4 shadow-lg h-full">
                     <div className="flex h-full">
                       <div className="flex-grow flex flex-wrap gap-4 items-center justify-center">
-                        <div className="w-full h-fit gap-y-10 grid grid-cols-3 p-10 pl-14">
+                        <div className="w-full h-fit xl:gap-y-10 grid grid-cols-3 xl:p-10 xl:pl-14 gap-y-4">
                           {navdata.navGraphql?.data?.navbar?.data?.attributes?.[
                             category
                           ]?.map(
                             (item: any) =>
                               item.__typename !== "ComponentCardCard" && (
                                 <div
-                                  className="col-span-1 h-fit items-center place-items-center text-base"
+                                  className="col-span-1 h-fit text-sm"
                                   key={item.id}
                                 >
                                   {item.icon?.data?.attributes?.url ? (
@@ -240,7 +247,7 @@ const Nav = () => {
                           )}
                         </div>
                       </div>
-                      <div className="max-w-[500px] h-full" id="featured-post">
+                      <div className="xl:max-w-[500px] max-w-[400px] h-full" id="featured-post">
                         {navdata.navGraphql?.data?.navbar?.data?.attributes?.[
                           category
                         ]?.find(
@@ -256,7 +263,7 @@ const Nav = () => {
                                     item.__typename === "ComponentCardCard"
                                 ).bgImage.data.attributes.url
                               }
-                              className="h-fit w-[30rem] mx-auto object-cover"
+                              className="h-fit w-[30rem] mx-auto object-contain"
                               alt={
                                 navdata.navGraphql.data.navbar.data.attributes[
                                   category
@@ -266,8 +273,8 @@ const Nav = () => {
                                 ).title
                               }
                             />
-                            <figcaption className="text-white absolute bottom-0 gap-2 z-30 p-4">
-                              <div className="font-semibold text-xl font-poppins">
+                            <figcaption className="text-white absolute bottom-0 gap-2 z-30 xl:p-4 lg:p-2">
+                              <div className="font-semibold xl:text-xl text-lg font-poppins">
                                 {
                                   navdata.navGraphql.data.navbar.data.attributes[
                                     category
@@ -277,7 +284,7 @@ const Nav = () => {
                                   ).title
                                 }
                               </div>
-                              <div className="font-montserrat text-sm max-h-14 text-ellipsis">
+                              <div className="font-montserrat xl:text-sm text-xs max-h-14 text-ellipsis">
                                 {navdata.navGraphql.data.navbar.data.attributes[
                                   category
                                 ]
@@ -338,8 +345,108 @@ const Nav = () => {
           ></div>
         </div>):(<div></div>)}
       </nav>
+      <nav className="fixed lg:hidden top-0 z-50 w-full bg-nav-dark pt-2 pb-1 h-16 flex px-4">
+        <Link to="/" prefetch="intent" className=" mr-auto flex">
+              <div className="flex flex-row justify-center items-center object-contain">
+                
+                  <img
+                    src={ivurl}
+                    alt="iVoyant Logo"
+                    className="aspect-video h-14 object-contain"
+                  />
+              </div>
+          </Link>
+          <div className="flex relative">
+            <button onClick={handleHamburgerClick}>
+              {sidebarOpen ? (
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M18 7.05L16.95 6L12 10.95L7.05 6L6 7.05L10.95 12L6 16.95L7.05 18L12 13.05L16.95 18L18 16.95L13.05 12L18 7.05Z" fill="url(#paint0_linear_7960_52353)"/>
+                <defs>
+                <linearGradient id="paint0_linear_7960_52353" x1="6" y1="6" x2="18.3047" y2="7.28271" gradientUnits="userSpaceOnUse">
+                <stop offset="0.00621719" stop-color="#B9C1EC"/>
+                <stop offset="1" stop-color="#A3B1FF"/>
+                </linearGradient>
+                </defs>
+                </svg>
+              ) : (
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M21 4.5H3V6h18V4.5Z" fill="url(#a)"/>
+                  <path d="M21 18H3v1.5h18V18Z" fill="url(#b)"/>
+                  <path d="M21 9H3v1.5h18V9Z" fill="url(#c)"/>
+                  <path d="M21 13.5H3V15h18v-1.5Z" fill="url(#d)"/>
+                  <defs>
+                    <linearGradient id="a" x1="3" y1="4.5" x2="21.37" y2="6.798" gradientUnits="userSpaceOnUse">
+                      <stop offset=".006" stopColor="#B9C1EC"/>
+                      <stop offset="1" stopColor="#A3B1FF"/>
+                    </linearGradient>
+                    <linearGradient id="b" x1="3" y1="4.5" x2="21.37" y2="6.798" gradientUnits="userSpaceOnUse">
+                      <stop offset=".006" stopColor="#B9C1EC"/>
+                      <stop offset="1" stopColor="#A3B1FF"/>
+                    </linearGradient>
+                    <linearGradient id="c" x1="3" y1="4.5" x2="21.37" y2="6.798" gradientUnits="userSpaceOnUse">
+                      <stop offset=".006" stopColor="#B9C1EC"/>
+                      <stop offset="1" stopColor="#A3B1FF"/>
+                    </linearGradient>
+                    <linearGradient id="d" x1="3" y1="4.5" x2="21.37" y2="6.798" gradientUnits="userSpaceOnUse">
+                      <stop offset=".006" stopColor="#B9C1EC"/>
+                      <stop offset="1" stopColor="#A3B1FF"/>
+                    </linearGradient>
+                  </defs>
+                </svg>
+              )}
+                
+            </button>
+          </div>
+          <div className={`absolute w-full ${(sidebarOpen)? 'flex':'hidden'} lg:hidden flex-col justify-between left-0  bg-haiti h-fit gap-10 screen-height text-gray-200 p-4 z-[999]`}>
+            <div className="flex ">
+              <div className="flex flex-col w-fit gap-5">
+                <div className="w-fit mx-auto gap-10 grid">
+                  {
+                    categories.map((category,index)=>(
+                        <button className="capitalize text-left font-montserrat text-xl font-semibold text-gray-200" key={index} onClick={() =>{handleToggle(index)}}>{category}</button>
+                    ))
+                  }
+                </div>
+              </div>
+              <div className="h-full w-fit text-white ml-10 max-h-80 overflow-y-scroll transition-opacity mr-auto">
+                {
+                    categories.map((category,index)=>(
+                      <div className="gap-5 grid">
+                        {
+                          navdata.navGraphql?.data?.navbar?.data?.attributes?.[category].map((item: any, linkindex: number) => (
+                            item?.name !== undefined && (
+                              <Link onClick={handleHamburgerClick} key={linkindex} className={`${(index === clicked) ? 'font-base font-montserrat text-base leading-5 tracking-wide' : 'hidden'}`} to={item?.link}>
+                                {item.name}
+                              </Link>
+                            )
+                          ))
+                        }
+                      </div>
 
-    </div>
+                    ))
+                  }
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <div className="flex gap-6">
+                <a onClick={handleHamburgerClick} className="flex" >
+                  <img src={yt} className="w-8 h-8" />
+                </a>
+                <a onClick={handleHamburgerClick} className="flex" >
+                  <img src={linkedin} className="w-8 h-8" />
+                </a>
+                <a onClick={handleHamburgerClick} className="flex" >
+                  <img src={fb} className="w-8 h-8" />
+                </a>
+                <a onClick={handleHamburgerClick} className="flex" >
+                  <img src={twitter} className="w-8 h-8" />
+                </a>
+              </div>
+              <small className="capitalize font-poppins text-gray-200 font-light leading-4">&#169; 2023 iVoyant All Rights Reserved</small>
+            </div>
+          </div>
+      </nav>
+    </>
   );
 };
 
